c3e18d8a0bc84e342a938c5bd53dcf8d0a1c32d7
ben <bjelinek42@gmail.com>
2022-09-07 15:19:34 -0600

    through lesson 18

---
+++ src/Footer.js
@@ -0,0 +1,5 @@
+export default function Footer() {
+  return (
+    <footer className="footer">Copyright 2022 Jelinek development. All rights reserved.</footer>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -3,30 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import Header from "./Header.js";
-
-function Footer() {
-  return (
-    <footer className="footer">Copyright 2022 Jelinek development. All rights reserved.</footer>
-  )
-}
-function Content() {
-  return (
-    <div>
-      <h1>Why I want to learn React</h1>
-      <ol>
-        <li>I want to get a job</li>
-        <li>React is more marketable and easier to use than Vue</li>
-        <li>Components are interesting</li>
-      </ol>
-    </div>
-  )
-}
+import MainContent from "./MainContent.js";
+import Footer from "./Footer.js"
 
 function Page() {
   return (
     <div>
       <Header />
-      <Content />
+      <MainContent />
       <Footer />
     </div>
   )
