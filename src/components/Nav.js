0e6224941192dec40f8fef779b3fdd9830a03e72
ben <bjelinek42@gmail.com>
2022-09-08 11:37:44 -0600

    navbar styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,15 @@
 import React from 'react'
-import reactLogo from "../images/react-logo.png";
+import reactLogo from "../images/React-icon.svg.png";
 import "../style.css"
 
 export default function Header() {
   return (
-    <header>
-      <nav className="nav">
-        <img className="image" src={{ reactLogo }} alt='some value' />
-        <ul className="nav-items">
-          <li>Pricing</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    </header>
+    <nav className="nav">
+      <img className="nav--icon" src={reactLogo} alt='some value' />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <h4 className="nav--title">
+        React Course - Project 1
+      </h4>
+    </nav>
   )
 }
